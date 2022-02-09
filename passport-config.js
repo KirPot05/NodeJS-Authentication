cac3ab07b4a372adbbcf86671680433497a12b98
Kiran <kiran0503@gmail.com>
2022-02-09 12:18:44 +0530

    Deploying Code to GitHub

---
+++ passport-config.js
@@ -0,0 +1,39 @@
+import passport_local from 'passport-local';
+import {compare} from 'bcrypt';
+
+const LocalStrategy = passport_local.Strategy;
+
+function initialize(passport, getUserByEmail, getUserById){
+
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+
+        if(user == null){
+            return done(null, false, {message: "No user present with these credentials"});
+        }
+
+        try{
+            if(await compare(password, user.hashedPassword)){
+                return done(null, user);
+            } 
+            
+            else{
+                return done(null, false, {message: "Password Incorrect"});
+            }
+
+
+        } catch(e) {
+            return done(e);
+        }
+    }
+
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => { 
+        return done(null, getUserById(id))
+    })
+
+}
+
+export default initialize;
